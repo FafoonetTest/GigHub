7c61f7ab466e30b758f663257141a9cff027c3b1
fafoonet <mf.flores@free.fr>
2018-11-20 23:03:11 +0100

    Implement view Gigs I'm attending

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using GigHub.ViewModels;
 using System;
 using System.Data.Entity;
 
@@ -23,7 +24,14 @@ namespace GigHub.Controllers
                 .Include(x => x.Genre)
                 .Where(x => x.Datetime > DateTime.Now);
 
-            return View(upcomingGigs);
+            var viewModel = new GigsViewModel
+            {
+                UpcomingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Upcoming Gigs"
+            };
+
+            return View("Gigs", viewModel);
         }
 
         public ActionResult About()
