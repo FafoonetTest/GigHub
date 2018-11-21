9b4263936ab79a1aefe7fa75617224fb7c783809
fafoonet <mf.flores@free.fr>
2018-11-21 22:44:03 +0100

    Implementation My Upcoming Gigs

---
+++ GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,31 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext(); 
+        }
+
+        [System.Web.Http.HttpPost]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
