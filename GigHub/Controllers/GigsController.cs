d398cbe994de09a701d060f39b0a26a0f31e5500
fafoonet <mf.flores@free.fr>
2018-11-23 22:16:23 +0100

    beginning implementation of notifications

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -22,7 +22,9 @@ namespace GigHub.Controllers
         {
             var userId = User.Identity.GetUserId();
             var gigs = _context.Gigs
-                .Where(g => g.ArtistId == userId && g.Datetime > DateTime.Now)
+                .Where(g => g.ArtistId == userId && 
+                            g.Datetime > DateTime.Now && 
+                            !g.IsCanceled)
                 .Include(g => g.Genre)
                 .ToList();
 
